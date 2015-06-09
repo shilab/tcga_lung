b04ca2c6b06ca6f53de7b00c5b1f9647df442982
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-06-09 09:48:27 -0400

    Refactored for testing

--- ExpressionMatrix.py
+++ ExpressionMatrix.py
@@ -2,33 +2,45 @@ from __future__ import print_function
 import sys
 import glob
 
+def createHeader(files):
+    #Will need to change IDs
+    header = "ID\t"
+    for file in files:
+        id = file.split('-')[0].split('unc.edu.')[-1]
+        header = header + id + "\t"
+    header = header.rstrip()
+    return header
+
+def parseGenes(files):
+    geneData={}
+    geneID=[]
+    for file in files:
+        genes = open(file, 'r').read()
+        genes = genes.split('\n')
+        for gene in genes:
+            if not (gene.startswith('gene') or gene.startswith('?')) and gene != '':
+                id, value = gene.split('\t')
+                id = id.split('|')[0]
+                if id in geneData:
+                    geneData[id]=geneData[id] + "\t" + value
+                else:
+                    geneData[id]=value
+                    geneID.append(id)    
+
+    return(geneData,geneID)
+
 #search argument needs to be quoted so it isn't expanded in the command line
-search = sys.argv[1]
-files = glob.glob(search)
+def main():
+    search = sys.argv[1]
+    files = glob.glob(search)
 
-geneData = {}
-geneID = []
+    output = createHeader(files)
 
-output="ID\t"
+    geneData, geneID = parseGenes(files)
 
-for file in files:
-    #Will need to change when aliquot ids are replaced by sample ids
-    id = file.split('-')[0].split('unc.edu.')[-1]
+    print(output)
+    for gene in geneID:
+        print(gene + '\t' + geneData[gene])
 
-    output = output+id+"\t"
-
-    genes = open(file, 'r').read()
-    genes = genes.split('\n')
-    for gene in genes:
-        if not (gene.startswith('gene') or gene.startswith('?')) and gene != '':
-            id, value = gene.split('\t')
-            id = id.split('|')[0]
-            if id in geneData:
-                geneData[id]=geneData[id] + "\t" + value
-            else:
-                geneData[id]=value
-                geneID.append(id)
-
-print(output)
-for gene in geneID:
-    print(gene + '\t' + geneData[gene])
+if __name__ == "__main__":
+    main()
