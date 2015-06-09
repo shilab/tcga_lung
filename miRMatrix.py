a13c3c9197c5ecc008f75a0777ae8cb6b5296cb6
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-06-09 11:06:16 -0400

    Refactored for testing

--- miRMatrix.py
+++ miRMatrix.py
@@ -4,31 +4,47 @@ import glob
 
 #TODO: Will have to check TCGA barcoding standard for 01A vs 01B vs 11A etc
 
-search = sys.argv[1]
-files = glob.glob(search)
+def createHeader(files):
+    header="ID\t"
+    
+    for file in files:
+        if file.split('-')[3]=="01A":
+            id = file.split('-')[2]
+        else:
+            continue
+        header = header + id + "\t"
+    header = header.rstrip()
+    return(header)
 
-output="ID\t"
-miRData={}
-miRID = []
-for file in files:
-    if file.split('-')[3]=="01A":
-        id = file.split('-')[2]
-    else:
-        continue
+def parsemiR(files):
+    miRData={}
+    miRID = []
 
-    output=output+id+"\t"
+    for file in files:
+        miR_file = open(file, 'r').read()
+        miRs = miR_file.split('\n')
+        for miR in miRs:
+            if (not miR.startswith('miR')) and miR != '':
+                id,_,value,_ = miR.split('\t')
+                if id in miRData:
+                    miRData[id]=miRData[id] + "\t" + value
+                else:
+                    miRData[id]=value
+                    miRID.append(id)
+    
+    return(miRData,miRID)
 
-    miR_file = open(file, 'r').read()
-    miRs = miR_file.split('\n')
-    for miR in miRs:
-        if (not miR.startswith('miR')) and miR != '':
-            id,_,value,_ = miR.split('\t')
-            if id in miRData:
-                miRData[id]=miRData[id] + "\t" + value
-            else:
-                miRData[id]=value
-                miRID.append(id)
+def main():
+    search = sys.argv[1]
+    files = glob.glob(search)
 
-print(output.rstrip())
-for miR in miRID:
-    print(miR + '\t' + miRData[miR])
+    output = createHeader(files)
+
+    miRData, miRID = parsemiR(files)
+
+    print(output)
+    for miR in miRID:
+        print(miR + '\t' + miRData[miR])
+
+if __name__ == "__main__":
+    main()
