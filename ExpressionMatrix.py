5743415771a45be9f95f5f4480391b59f205981f
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-06-10 08:12:06 -0400

    Reformatted for better PEP-8

--- ExpressionMatrix.py
+++ ExpressionMatrix.py
@@ -2,45 +2,45 @@ from __future__ import print_function
 import sys
 import glob
 
-def createHeader(files):
+def create_header(files):
     #Will need to change IDs
     header = "ID\t"
-    for file in files:
-        id = file.split('-')[0].split('unc.edu.')[-1]
-        header = header + id + "\t"
+    for filename in files:
+        sample_id = filename.split('-')[0].split('unc.edu.')[-1]
+        header = header + sample_id + "\t"
     header = header.rstrip()
     return header
 
-def parseGenes(files):
-    geneData={}
-    geneID=[]
-    for file in files:
-        genes = open(file, 'r').read()
+def parse_genes(files):
+    gene_data = {}
+    gene_ids = []
+    for data in files:
+        genes = open(data, 'r').read()
         genes = genes.split('\n')
         for gene in genes:
             if not (gene.startswith('gene') or gene.startswith('?')) and gene != '':
-                id, value = gene.split('\t')
-                id = id.split('|')[0]
-                if id in geneData:
-                    geneData[id]=geneData[id] + "\t" + value
+                gene_id, value = gene.split('\t')
+                gene_id = gene_id.split('|')[0]
+                if gene_id in gene_data:
+                    gene_data[gene_id] = gene_data[gene_id] + "\t" + value
                 else:
-                    geneData[id]=value
-                    geneID.append(id)    
+                    gene_data[gene_id] = value
+                    gene_ids.append(gene_id)
 
-    return(geneData,geneID)
+    return(gene_data, gene_ids)
 
 #search argument needs to be quoted so it isn't expanded in the command line
 def main():
     search = sys.argv[1]
     files = glob.glob(search)
 
-    output = createHeader(files)
+    output = create_header(files)
 
-    geneData, geneID = parseGenes(files)
+    gene_data, gene_id = parse_genes(files)
 
     print(output)
-    for gene in geneID:
-        print(gene + '\t' + geneData[gene])
+    for gene in gene_id:
+        print(gene + '\t' + gene_data[gene])
 
 if __name__ == "__main__":
     main()
--- tests/test_ExpressionMatrix.py
+++ tests/test_ExpressionMatrix.py
@@ -2,14 +2,14 @@ from ExpressionMatrix import *
 import unittest
 
 def test_header():
-    assert createHeader(['unc.edu.ff74b4c6-e938-4a40-ab1a-84525349a62e.1229487.rsem.genes.normalized_results','unc.edu.8719f9af-0876-4ec6-a4dc-4178ad964349.1643074.rsem.genes.normalized_results']) == "ID\tff74b4c6\t8719f9af"
+    assert create_header(['unc.edu.ff74b4c6-e938-4a40-ab1a-84525349a62e.1229487.rsem.genes.normalized_results', 'unc.edu.8719f9af-0876-4ec6-a4dc-4178ad964349.1643074.rsem.genes.normalized_results']) == "ID\tff74b4c6\t8719f9af"
 
-def test_geneID():
-    _, geneID = parseGenes(['tests/genes1','tests/genes2'])
-    assert geneID == ['A1BG','A1CF','A2BP1']
+def test_gene_id():
+    _, gene_ids = parse_genes(['tests/genes1', 'tests/genes2'])
+    assert gene_ids == ['A1BG', 'A1CF', 'A2BP1']
 
-def test_geneData():
-    geneData, _ = parseGenes(['tests/genes1','tests/genes2'])
-    assert geneData['A1BG']=="39.7563\t39.7563"
-    assert geneData['A1CF']=="0.2593\t0.2593"
-    assert geneData['A2BP1']=="0.0000\t0.0000"
+def test_gene_ata():
+    gene_data, _ = parse_genes(['tests/genes1', 'tests/genes2'])
+    assert gene_data['A1BG'] == "39.7563\t39.7563"
+    assert gene_data['A1CF'] == "0.2593\t0.2593"
+    assert gene_data['A2BP1'] == "0.0000\t0.0000"
