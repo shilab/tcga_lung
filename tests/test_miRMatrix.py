4859d661f02070e524d085cb7bd0f80b7f8f34e2
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-06-11 08:13:29 -0400

    Reformatted to fit changes in miRMatrix

--- tests/test_miRMatrix.py
+++ tests/test_miRMatrix.py
@@ -2,15 +2,15 @@ from miRMatrix import *
 import unittest
 
 def test_header():
-    assert createHeader(['TCGA-55-6985-01A-11H-1948-13.mirna.quantification.txt','TCGA-S2-AA1A-01A-12H-A39B-13.mirna.quantification.txt']) == "ID\t6985\tAA1A"
-    assert createHeader(['TCGA-55-6985-01A-11H-1948-13.mirna.quantification.txt','TCGA-S2-AA1A-11A-12H-A39B-13.mirna.quantification.txt']) == "ID\t6985"
+    assert create_header(['TCGA-55-6985-01A-11H-1948-13.mirna.quantification.txt','TCGA-S2-AA1A-01A-12H-A39B-13.mirna.quantification.txt']) == "ID\t6985\tAA1A"
+    assert create_header(['TCGA-55-6985-01A-11H-1948-13.mirna.quantification.txt','TCGA-S2-AA1A-11A-12H-A39B-13.mirna.quantification.txt']) == "ID\t6985"
 
-def test_mirID():
-    _, mirID = parsemiR(['tests/miRNA1','tests/miRNA2'])
-    assert mirID == ['hsa-let-7a-1','hsa-let-7a-2','hsa-let-7a-3']
+def test_mirna_ids():
+    _, mirna_ids = parse_mirna(['tests/miRNA1','tests/miRNA2'])
+    assert mirna_ids == ['hsa-let-7a-1','hsa-let-7a-2','hsa-let-7a-3']
 
-def test_miRData():
-    miRData, _ = parsemiR(['tests/miRNA1','tests/miRNA2'])
-    assert miRData['hsa-let-7a-1']=="9870.569273\t3977.302846"
-    assert miRData['hsa-let-7a-2']=="19756.504636\t7969.303424"
-    assert miRData['hsa-let-7a-3']=="9926.316021\t4004.529793"
+def test_mirna_data():
+    mirna_data, _ = parse_mirna(['tests/miRNA1','tests/miRNA2'])
+    assert mirna_data['hsa-let-7a-1'] == "9870.569273\t3977.302846"
+    assert mirna_data['hsa-let-7a-2'] == "19756.504636\t7969.303424"
+    assert mirna_data['hsa-let-7a-3'] == "9926.316021\t4004.529793"
