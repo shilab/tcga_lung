edd0341c9c4e7a5426b3637f0167cd8430c2979b
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-06-11 08:13:04 -0400

    Reformatted for better PEP-8 compliance

--- miRMatrix.py
+++ miRMatrix.py
@@ -4,47 +4,47 @@ import glob
 
 #TODO: Will have to check TCGA barcoding standard for 01A vs 01B vs 11A etc
 
-def createHeader(files):
-    header="ID\t"
-    
-    for file in files:
-        if file.split('-')[3]=="01A":
-            id = file.split('-')[2]
+def create_header(files):
+    header = "ID\t"
+
+    for filename in files:
+        if filename.split('-')[3] == "01A":
+            sample_id = filename.split('-')[2]
         else:
             continue
-        header = header + id + "\t"
+        header = header + sample_id + "\t"
     header = header.rstrip()
-    return(header)
+    return header
 
-def parsemiR(files):
-    miRData={}
-    miRID = []
+def parse_mirna(files):
+    mirna_data = {}
+    mirna_ids = []
 
-    for file in files:
-        miR_file = open(file, 'r').read()
-        miRs = miR_file.split('\n')
-        for miR in miRs:
-            if (not miR.startswith('miR')) and miR != '':
-                id,_,value,_ = miR.split('\t')
-                if id in miRData:
-                    miRData[id]=miRData[id] + "\t" + value
+    for filename in files:
+        mirna_file = open(filename, 'r').read()
+        mirnas = mirna_file.split('\n')
+        for mirna in mirnas:
+            if (not mirna.startswith('miR')) and mirna != '':
+                mirna_id, _, value, _ = mirna.split('\t')
+                if mirna_id in mirna_data:
+                    mirna_data[mirna_id] = mirna_data[mirna_id] + "\t" + value
                 else:
-                    miRData[id]=value
-                    miRID.append(id)
-    
-    return(miRData,miRID)
+                    mirna_data[mirna_id] = value
+                    mirna_ids.append(mirna_id)
+
+    return(mirna_data, mirna_ids)
 
 def main():
     search = sys.argv[1]
     files = glob.glob(search)
 
-    output = createHeader(files)
+    output = create_header(files)
 
-    miRData, miRID = parsemiR(files)
+    mirna_data, mirna_ids = parse_mirna(files)
 
     print(output)
-    for miR in miRID:
-        print(miR + '\t' + miRData[miR])
+    for mirna in mirna_ids:
+        print(mirna + '\t' + mirna_data[mirna])
 
 if __name__ == "__main__":
     main()
